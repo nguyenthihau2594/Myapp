5a605158cb48f17567ecb25623e9d2540a078e57
haunguyen <haunguyen@hivelab.com>
2022-09-26 17:13:18 +0700

    update

--- src/components/component/LNB/LNB.tsx
+++ src/components/component/LNB/LNB.tsx
@@ -4,17 +4,26 @@ import Myinfo from "../Myinfo/Myinfo";
 import s from "./LNB.module.scss";
 
 import Logo from "../../../asset/images/thumbnail/logo.png";
+import Logo_mo from "../../../asset/images/thumbnail/logo_mo.png";
+import { ReactComponent as IconMenu } from "../../../asset/images/icons/ico_menu.svg";
+import { ReactComponent as IconClose } from "../../../asset/images/icons/ico_close.svg";
 
 
 function LNB() {
 	return (
 		<div className={s.lnb}>
-			<h1 className={s.logo}><a href="#none"><img src={Logo} alt="" /></a></h1>
-			<div className={s.lnb__info}>
-				<Myinfo isLogin={true} name="nguyenhau" level={5} />
-			</div>
-			<div className={s.lnb__menu}>
-				<LNBMenu />
+			<div className={s.lnb_inner}>
+				<h1 className={s.logo}><a href="#none"><img src={Logo} alt="" className="is_pc" /><img src={Logo_mo} alt="" className="is_mo" /></a></h1>
+				<button className={s.btn_menu}><IconMenu /></button>
+				<div className={s.lnb_content}>
+					<button className={s.btn_close}><IconClose /></button>
+					<div className={s.lnb__info}>
+						<Myinfo isLogin={true} name="nguyenhau" level={5} />
+					</div>
+					<div className={s.lnb__menu}>
+						<LNBMenu />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
